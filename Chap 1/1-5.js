8d440152418b0568230378b6e3ebc3d609f66764
ChaeEunOh <grace2097@naver.com>
2024-04-30 19:44:27 +0900

    Reallocation of recommended data

--- Chap 1/1-5.js
+++ Chap 1/1-5.js
@@ -1,11 +1,6 @@
-var a = 10;
-var b = a;
-
-var obj1 = {c: 10, d: 'ddd'}
-var obj2 = obj1;
-
-b = 15;
-obj2.c = 20;
-
-console.log(a == b);
-console.log(obj1 == obj2);
+var obj1 = {
+    a: 1,
+    b: 'bbb',
+  };
+  obj1.a = 2;
+  
